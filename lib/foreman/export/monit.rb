af404e19e150436cd67818a237a0c71fbc0f02fb
gropher <grophen@gmail.com>
2013-01-04 20:30:57 +0400

    Monit export updated

--- lib/foreman/export/monit.rb
+++ lib/foreman/export/monit.rb
@@ -1,44 +1,47 @@
 require 'foreman/export'
 require 'foreman/cli'
 
-module Foreman
-  module Export
-    class Monit < Foreman::Export::Base
-      attr_reader :pid, :check
+class Foreman::Export::Monit < Foreman::Export::Base
+  attr_reader :pid, :check
 
-      def export
-        super
+  def export
+    super
 
-        @user ||= app
-        @log = File.expand_path("#{@location}/log")
-        @pid = File.expand_path("#{@location}/pids")
-        @check = File.expand_path("#{@location}/lock")
-        @location = File.expand_path(@location)
-        FileUtils.mkdir_p(@pid)
-        FileUtils.mkdir_p(@check)
+    @user ||= app
+    @log = File.expand_path("#{@location}/log")
+    @pid = File.expand_path("#{@location}/pids")
+    @check = File.expand_path("#{@location}/lock")
+    @location = File.expand_path(@location)
+    FileUtils.mkdir_p(@pid)
+    FileUtils.mkdir_p(@check)
 
-        engine.each_process do |name, process|
-          write_template "monit/wrapper.sh.erb", "#{app}-#{name}.sh", binding
-        end
-
-        write_template "monit/monitrc.erb", "#{app}.monitrc", binding
-      end
-
-      def wrapper_path_for(name)
-        File.join(location, "#{app}-#{name}.sh")
-      end
-
-      def pid_file_for(name, num)
-        File.join(pid, "#{name}-#{num}.pid")
-      end
-
-      def log_file_for(name, num)
-        File.join(log, "#{name}-#{num}.log")
-      end
-
-      def check_file_for(name)
-        File.join(check, "#{name}.restart")
-      end
+    engine.each_process do |name, process|
+      write_template "monit/wrapper.sh.erb", "#{app}-#{name}.sh", binding
+      chmod 0755, "#{app}-#{name}.sh"
+      `touch #{check_file_for(name)}`
     end
+
+    `touch #{app_check_file}`
+    write_template "monit/monitrc.erb", "#{app}.monitrc", binding
+  end
+
+  def wrapper_path_for(name)
+    File.join(location, "#{app}-#{name}.sh")
+  end
+
+  def pid_file_for(name, num)
+    File.join(pid, "#{app}-#{name}-#{num}.pid")
+  end
+
+  def log_file_for(name, num)
+    File.join(log, "#{app}-#{name}-#{num}.log")
+  end
+
+  def check_file_for(name)
+    File.join(check, "#{app}-#{name}.restart")
+  end
+
+  def app_check_file
+    File.join(check, "#{app}.restart")
   end
 end
